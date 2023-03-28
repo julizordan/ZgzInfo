6ef2ef959f51eda17b1c1d7e3d644db040e28730
julizordan <juliana.carolina@mango.com>
2023-03-28 19:52:12 +0200

    data base schemas

--- ZgzInfo_back/app.js
+++ ZgzInfo_back/app.js
@@ -1,13 +1,16 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
+var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.disable('x-powered-by','app_server','views');
+app.set('view engine', 'pug');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
