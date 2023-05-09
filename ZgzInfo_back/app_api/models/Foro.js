3ac46905c59863e0add1872eb5a7d02865e4550b
julizordan <juliana.carolina@mango.com>
2023-05-09 12:24:52 +0200

    fixed bug

--- ZgzInfo_back/app_api/models/Foro.js
+++ ZgzInfo_back/app_api/models/Foro.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const foroSchema = new mongoose.Schema({
     id: {type: String, required: true, unique: true},
     tipo: { type: String, required: true },
-    titulo: { type: String, required: true, unique:true },
+    titulo: { type: String, required: true },
     comentarios: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comentarios', default: []}]
 });
 
